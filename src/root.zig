ac0f3ca377b960edfc34728ffab27f922e152ed7
zzstoatzz <thrast36@gmail.com>
2026-01-03 18:11:36 -0600

    clean up root.zig

--- src/root.zig
+++ src/root.zig
@@ -2,21 +2,9 @@
 //!
 //! parsing and validation for AT Protocol string formats.
 
-// identifiers
 pub const Tid = @import("internal/tid.zig").Tid;
 pub const Did = @import("internal/did.zig").Did;
 pub const Handle = @import("internal/handle.zig").Handle;
 pub const Nsid = @import("internal/nsid.zig").Nsid;
 pub const Rkey = @import("internal/rkey.zig").Rkey;
-
-// uris
 pub const AtUri = @import("internal/at_uri.zig").AtUri;
-
-test {
-    _ = @import("internal/tid.zig");
-    _ = @import("internal/did.zig");
-    _ = @import("internal/handle.zig");
-    _ = @import("internal/nsid.zig");
-    _ = @import("internal/rkey.zig");
-    _ = @import("internal/at_uri.zig");
-}
